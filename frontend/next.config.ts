4f266a3a3904b79061eab0359eefa8e5b1e8172a
lucasolasz <olaszlucas072@gmail.com>
2026-05-09 17:34:12 -0300

    Permite rodar o projeto em localhost

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- allowedDevOrigins: ['192.168.1.33'],
+  allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.1.33"],
 };
 
 export default nextConfig;
