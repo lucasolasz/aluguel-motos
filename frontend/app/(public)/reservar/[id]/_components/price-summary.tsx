cdfd91d60d62099589149ff41192c835ee53f316
lucasolasz <olaszlucas072@gmail.com>
2026-05-16 22:58:03 -0300

    Corrige warning

--- frontend/app/(public)/reservar/[id]/_components/price-summary.tsx
+++ frontend/app/(public)/reservar/[id]/_components/price-summary.tsx
@@ -44,6 +44,7 @@ export function PriceSummary({
               fill
               sizes="(max-width: 768px) 100vw, 33vw"
               className="object-cover"
+              priority
             />
           </div>
           <div>
