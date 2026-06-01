402a5f983f279ab1d64c04d5773a4cf52d89a80a
lucasolasz <olaszlucas072@gmail.com>
2026-06-01 00:05:42 -0300

    Corrige ips para usar o app

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.1.33", "10.34.6.112"],
+  allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.1.33", "10.34.6.112", "192.168.1.221"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "bucketaluguelmotos.ltech.app.br" },
